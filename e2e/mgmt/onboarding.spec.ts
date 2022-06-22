05e6d6474b987782151d546f9e918c213b7b4510
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-06-22 15:05:08 +0800

    refactor: refactor onboarding spec to correctly handle baseUrl config

--- e2e/mgmt/onboarding.spec.ts
+++ e2e/mgmt/onboarding.spec.ts
@@ -3,11 +3,7 @@ import { test, expect } from "@playwright/test";
 test("first time enter user should go to the onboarding page", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-  await page.goto(process.env.NEXT_PUBLIC_MAIN_URL);
-
+  await page.goto("/");
   await expect(page).toHaveURL(
     `${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`
   );
@@ -16,11 +12,7 @@ test("first time enter user should go to the onboarding page", async ({
 test("should disable start button, if email input is not correct", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-
-  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.goto("/onboarding");
   await page.locator("#email").fill("instill");
   await page.type("#company_name", "instill-ai");
 
@@ -35,13 +27,8 @@ test("should disable start button, if email input is not correct", async ({
 test("should disable start button, if other field is not filled in", async ({
   page,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-
-  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.goto("/onboarding");
   await page.locator("#email").fill("instill");
-
   const startButtonState = await page.locator("text=Start").isDisabled();
   expect(startButtonState).toBeTruthy();
 });
@@ -50,11 +37,7 @@ test("should successfully fill in the onboarding form and submit", async ({
   page,
   context,
 }) => {
-  if (!process.env.NEXT_PUBLIC_MAIN_URL) {
-    throw new Error("env not found");
-  }
-
-  await page.goto(`${process.env.NEXT_PUBLIC_MAIN_URL}/onboarding`);
+  await page.goto("/onboarding");
   await page.locator("#email").fill("instill@gmail.com");
   await page.type("#company_name", "instill-ai");
 
@@ -68,7 +51,6 @@ test("should successfully fill in the onboarding form and submit", async ({
   // Click Start and wait for navigation
   await page.locator("text=Start").click();
   await page.waitForNavigation();
-
   expect(page.url()).toBe(`${process.env.NEXT_PUBLIC_MAIN_URL}/pipelines`);
 
   // Check cookie exist
